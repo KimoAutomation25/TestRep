8fa82299e3b367ca1f77bffb9d3004e70e3f1a02
KimoAutomation25 <gobara.abdelhakim12@gmail.com>
2025-10-02 22:09:24 -0400

    fourth commit

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
 #......................................... First Pyhton Test ............................................#
 tester_fname = "Abdelhakim Gobara"
 project_name = "CMS PMMOD"
+
+print(tester_fname + " ------> " + project_name)
